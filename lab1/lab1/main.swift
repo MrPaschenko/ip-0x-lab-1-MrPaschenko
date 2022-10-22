ac87fade780e3206b9ddb2458d65f21dff009f66
mrpaschenko <paschenkod2@gmail.com>
2022-10-22 22:55:14 +0200

    Implement reading file

--- lab1/lab1/main.swift
+++ lab1/lab1/main.swift
@@ -1,11 +1,16 @@
-//
-//  main.swift
-//  lab1
-//
-//  Created by Дмитрий Пащенко on 21.10.2022.
-//
-
 import Foundation
 
-print("Hello, World!")
+let string = try String(contentsOfFile: "/Users/mrpaschenko/Developer/ip-0x-lab-1-MrPaschenko/lab1/lab1/File.txt", encoding: String.Encoding.utf8)
+let array = string.split(separator: "\n")
 
+let numberOfGeneration = array[0]
+
+let size = array[1].split(separator: " ")
+
+var arrayMain:[[Any]] = []
+
+for i in 1...Int(size[1])! {
+    arrayMain.append(array[1 + i].map { String($0) })
+}
+
+print(arrayMain[0])
